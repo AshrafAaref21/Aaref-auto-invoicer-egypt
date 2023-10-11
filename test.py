2c7b638baa47d2140ff4310614e958a1feb075b9
Ashraf Aaref <82468597+AshrafAaref21@users.noreply.github.com>
2023-10-11 11:37:51 +0300

    Update test.py

--- test.py
+++ test.py
@@ -104,7 +104,7 @@ if s_p == 'Procurements':
 
         btn = st.download_button(
             "Press to Download",
-            df.to_csv(index=False).encode('windows-1256').decode('windows-1256'),
+            df.to_csv(index=False).encode('utf-8').decode('utf-8'),
             "Aaref.csv",
             "text/csv",
             key='download-csv')
@@ -179,7 +179,7 @@ elif s_p == 'Sales':
 
         btn = st.download_button(
             "Press to Download",
-            df.to_csv(index=False).encode('windows-1256').decode('windows-1256'),
+            df.to_csv(index=False).encode('utf-8').decode('utf-8'),
             "Aaref.csv",
             "text/csv",
             key='download-csv')
