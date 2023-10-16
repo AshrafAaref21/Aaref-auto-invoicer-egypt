be0f73c0ab047368ca67cd30d66f7feced52c8a3
Ashraf Aaref <82468597+AshrafAaref21@users.noreply.github.com>
2023-10-16 09:32:15 +0300

    Update test.py

--- test.py
+++ test.py
@@ -182,33 +182,34 @@ elif s_p == 'Sales':
             }
 
             bytes_data = uploaded_file.read()
-            # st.write("filename:", uploaded_file.name)
-            # st.write(bytes_data)
-            # f = open(bytes_data,encoding='utf-8')
-            data = json.loads(bytes_data)
-
-            dic['Reciever ID'] = data['receiverId']
-            dic['Internal ID'] = data['internalId']
-            dic['Reciever Name'] = u"{}".format(str(data['receiverName']))
-            dic['DateTime Issued'] = data['dateTimeIssued'][:10]
-            dic['DateTime Received'] = data['dateTimeReceived'][:10]
-            dic['Total Sales (EGP)'] = data['totalSales']
-            dic['Total discount (EGP)'] = data['totalDiscount']
-
-            dic['Total Amount (EGP)'] = data['total']
-
-    
-            doc_data = json.loads(str(BeautifulSoup(data['document'], 'html.parser')))
-
-            dic['Total Items Discount (EGP)'] = doc_data['totalItemsDiscountAmount']
-            dic['Extra Invoice Discounts (EGP)'] =  doc_data['extraDiscountAmount']
 
             try:
-                dic['Value added tax (EGP)'] = doc_data['taxTotals'][0]['amount']
+                data = json.loads(bytes_data)
+    
+                dic['Reciever ID'] = data['receiverId']
+                dic['Internal ID'] = data['internalId']
+                dic['Reciever Name'] = u"{}".format(str(data['receiverName']))
+                dic['DateTime Issued'] = data['dateTimeIssued'][:10]
+                dic['DateTime Received'] = data['dateTimeReceived'][:10]
+                dic['Total Sales (EGP)'] = data['totalSales']
+                dic['Total discount (EGP)'] = data['totalDiscount']
+    
+                dic['Total Amount (EGP)'] = data['total']
+    
+        
+                doc_data = json.loads(str(BeautifulSoup(data['document'], 'html.parser')))
+    
+                dic['Total Items Discount (EGP)'] = doc_data['totalItemsDiscountAmount']
+                dic['Extra Invoice Discounts (EGP)'] =  doc_data['extraDiscountAmount']
+    
+                try:
+                    dic['Value added tax (EGP)'] = doc_data['taxTotals'][0]['amount']
+    
+                except IndexError:
+                    dic['Value added tax (EGP)'] = 0
 
-            except IndexError:
-                dic['Value added tax (EGP)'] = 0
-                # st.write(doc_data)
+            except:
+                st.error('Please Choose Procurements Type')
 
             
 
