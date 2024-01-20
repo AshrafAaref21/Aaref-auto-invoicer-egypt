1f8a41fd2a7ee3096d435cab1357fda97ea6830d
Ashraf Aaref <82468597+AshrafAaref21@users.noreply.github.com>
2024-01-20 17:01:33 +0200

    Update test.py

--- test.py
+++ test.py
@@ -13,7 +13,7 @@ import xmltojson
 HtmlFile = open("ads.html", 'r', encoding='utf-8')
 source_code = HtmlFile.read()
 # print(source_code)
-components.html(source_code, height=600)
+st.components.html(source_code, height=600)
 
 
 
